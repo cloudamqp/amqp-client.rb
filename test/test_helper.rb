cfde5235eb7768c1501ceb1821d105fc8e975b9b
Patrik Ragnarsson <patrik@starkast.net>
2021-11-21 22:04:15 +0100

    CI: Time out individual tests, allow JRuby/TruffleRuby to fail (#14)
    * Time out individual test cases
    
    Code adapted from Puma's test suite: https://github.com/puma/puma/blob/8833ca4bda02c23b71624bbadd1b3cc095c6b1bf/test/helper.rb#L70-L101
    
    * Allow JRuby/TruffleRuby to fail

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,29 @@ require "amqp/client"
 require "minitest/autorun"
 
 Thread.abort_on_exception = true
+
+require "timeout"
+module TimeoutEveryTestCase
+  # our own subclass so we never confused different timeouts
+  class TestTimeout < Timeout::Error
+    def self.limit
+      60
+    end
+  end
+
+  def run
+    capture_exceptions do
+      ::Timeout.timeout(TestTimeout.limit,
+                        TestTimeout,
+                        "timed out after #{TestTimeout.limit} seconds") do
+        before_setup
+        setup
+        after_setup
+        send(name)
+      end
+    end
+    Minitest::Result.from(self)
+  end
+end
+
+Minitest::Test.prepend TimeoutEveryTestCase
