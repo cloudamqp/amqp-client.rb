35de443802641b0484b9f10c07d4eb82be5f4495
Anders Bälter <anders@84codes.com>
2025-09-15 14:29:13 +0200

    Support binding with high level objects

--- lib/amqp/client/exchange.rb
+++ lib/amqp/client/exchange.rb
@@ -4,6 +4,8 @@ module AMQP
   class Client
     # High level representation of an exchange
     class Exchange
+      attr_reader :name
+
       # Should only be initialized from the Client
       # @api private
       def initialize(client, name)
@@ -14,7 +16,7 @@ module AMQP
       # Publish to the exchange
       # @param body [String] The message body
       # @param routing_key [String] The routing key of the message,
-      #   the exchange may use this when routing the message to bound queues
+      #   the exchange may use this when routing the message to bound queues (defaults to empty string)
       # @param properties [Properties]
       # @option properties [String] content_type Content type of the message body
       # @option properties [String] content_encoding Content encoding of the body
@@ -30,28 +32,30 @@ module AMQP
       # @option properties [String] user_id Can be used to verify that this is the user that published the message
       # @option properties [String] app_id Can be used to indicates which app that generated the message
       # @return [Exchange] self
-      def publish(body, routing_key, **properties)
+      def publish(body, routing_key = "", **properties)
         @client.publish(body, @name, routing_key, **properties)
         self
       end
 
       # Bind to another exchange
-      # @param exchange [String] Name of the exchange to bind to
-      # @param binding_key [String] Binding key on which messages that match might be routed (depending on exchange type)
+      # @param source [String, Exchange] Name of the exchange to bind to, or the exchange object itself
+      # @param binding_key [String] Binding key on which messages that match might be routed (defaults to empty string)
       # @param arguments [Hash] Message headers to match on (only relevant for header exchanges)
       # @return [Exchange] self
-      def bind(exchange, binding_key, arguments: {})
-        @client.exchange_bind(@name, exchange, binding_key, arguments: arguments)
+      def bind(source, binding_key = "", arguments: {})
+        source = source.is_a?(String) ? source : source.name
+        @client.exchange_bind(@name, source, binding_key, arguments: arguments)
         self
       end
 
       # Unbind from another exchange
-      # @param exchange [String] Name of the exchange to unbind from
-      # @param binding_key [String] Binding key which the queue is bound to the exchange with
+      # @param source [String, Exchange] Name of the exchange to unbind from, or the exchange object itself
+      # @param binding_key [String] Binding key which the queue is bound to the exchange with (defaults to empty string)
       # @param arguments [Hash] Arguments matching the binding that's being removed
       # @return [Exchange] self
-      def unbind(exchange, binding_key, arguments: {})
-        @client.exchange_unbind(@name, exchange, binding_key, arguments: arguments)
+      def unbind(source, binding_key = "", arguments: {})
+        source = source.is_a?(String) ? source : source.name
+        @client.exchange_unbind(@name, source, binding_key, arguments: arguments)
         self
       end
 
